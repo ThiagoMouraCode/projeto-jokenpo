b15cda241cfe7ce022acdeb3b1fa2049b20cb7c0
ThiagoDesWEb <thiagomooura@gmail.com>
2025-10-21 20:58:36 -0300

    trabalhando com switch

--- jokenpo.c
+++ jokenpo.c
@@ -4,6 +4,7 @@
 
 int main() {
     int escolhaJogador, escolhaComputador;
+    srand(time(0));
 
     printf("*** Jogo de Jokenpô ***\n");
     printf("Escolha uma opção\n");
@@ -12,6 +13,37 @@ int main() {
     printf("3. Tesoura\n");
     printf("Escolha: ");
     scanf("%d", &escolhaJogador);
+
+    escolhaComputador = rand() % 3 + 1;
+
+    switch (escolhaJogador)
+    {
+    case 1:
+        printf("Jogador: Pedra - ");
+        break;
+    case 2:
+        printf("Jogador: Papel - ");
+        break;
+    case 3:
+        printf("Jogador: Tesoura - ");
+        break;
+    default:
+        printf("Opção inválida");
+        break;
+    }
+
+    switch (escolhaComputador)
+    {
+    case 1:
+        printf("Computador: Pedra\n");
+        break;
+    case 2:
+        printf("Computador: Papel\n");
+        break;
+    case 3:
+        printf("Computador: Tesoura\n");
+        break;
+    }
 
     return 0;
 }    
