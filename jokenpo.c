bb2ccd38da8663fc5b99dcf89990949439e97b4a
ThiagoDesWEb <thiagomooura@gmail.com>
2025-10-21 20:56:42 -0300

    menu do jogo

--- jokenpo.c
+++ jokenpo.c
@@ -5,7 +5,13 @@
 int main() {
     int escolhaJogador, escolhaComputador;
 
+    printf("*** Jogo de Jokenpô ***\n");
+    printf("Escolha uma opção\n");
+    printf("1. Pedra\n");
+    printf("2. Papel\n");
+    printf("3. Tesoura\n");
+    printf("Escolha: ");
+    scanf("%d", &escolhaJogador);
 
-    
-return 0;
+    return 0;
 }    
